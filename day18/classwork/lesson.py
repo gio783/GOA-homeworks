a506a5c7492a364cb217d2e8454b140f05f1a468
unknown <giokhorbala13@gmail.com>
2025-06-19 13:51:49 +0400

    nineteenth commit

---
+++ day18/classwork/lesson.py
@@ -0,0 +1,11 @@
+my_list = [1, 2, "a", "b", 1.1, 2.2, True, False]
+
+ints = [1, 2]
+strings = ["a", "b"]
+floats = [1.1, 2.2]
+bools = [True, False]
+
+print(ints)
+print(strings)
+print(floats)
+print(bools)
---
+++ day18/homework/lesson3.py
@@ -0,0 +1,4 @@
+data_types_unique = [100, 3.14, "ტექსტი", True]
+
+
+print("String მნიშვნელობა:", data_types_unique[-2])
