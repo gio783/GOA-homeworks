4230d85c0f6489f987fc11ca370d25d49ddcb4c7
unknown <giokhorbala13@gmail.com>
2025-10-26 20:51:33 +0400

    thirtyfivth commit

---
+++ day34/classwork/lesson1.py
@@ -0,0 +1,19 @@
+
+students = ["გიორგი", "ნინო", "ლუკა", "გივი", "ვატო"]
+print("students")
+
+
+students.append("დავით")
+print("append() ", students)
+
+
+students.pop()  
+print("pop() ბოლო ელემენტის წაშლა:", students)
+
+print(" სიგრძე:", len(students))
+
+
+students.append("ნინო")
+print("ნინო რამდენჯერ არის სიაში:", students.count("ნინო"))
+
+
---
+++ day34/classwork/lesson2.py
@@ -0,0 +1,20 @@
+fruits = {"ვაშლი", "ბანანი", "მანგო", "ატამი"}
+print(" set:", fruits)
+
+
+fruits.add("მსხალი")
+print("add() ", fruits)
+
+
+fruits.remove("ბანანი")
+print("remove() ", fruits)
+
+
+fruits.discard("ატამი")
+print("discard() ", fruits)
+
+
+removed = fruits.pop()
+print( removed)
+print("pop()", fruits)
+
